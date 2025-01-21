faea68c22ac8a1b28fdef36a91473af781a654a1
ali <muhammadalihassanoffiial@gmail.com>
2025-01-21 19:38:20 +0500

    feat: add server class to manage server lifecycle

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,54 @@
 import { FastifyInstance } from "fastify";
 import initializeFastify from "./app/app";
 
+class Server {
+  public constructor(private readonly app: FastifyInstance) {}
+
+  public start = async () => {
+    this.app.log.info("Starting Server....");
+    const { HOST, PORT } = this.app.config;
+    const address = await this.app.listen({ host: HOST, port: parseInt(PORT) });
+    this.app.log.info(`Server listening at ${address}`);
+  };
+
+  public shutdown = async () => {
+    this.app.log.info("Gracefully shutting down...");
+    await this.app.close();
+    process.exit();
+  };
+}
 
 
 async function main() {
-  let fastify: FastifyInstance|null = null;
+  let server: Server | null = null;
+  let fastify: FastifyInstance | null = null;
   try {
     fastify = await initializeFastify();
-    const port = parseInt(fastify.config.PORT ?? 3000);
-    const host = "0.0.0.0";
-    const address = await fastify.listen({ port, host });
-    fastify.log.info(`Server listening at ${address}`);
+    server = new Server(fastify);
+    await server.start();
   } catch (err: unknown) {
-    if (fastify) {
-      fastify.log.error(err);
-    } else {
-      console.error("Failed to Initialize Fastify", err);
-    }
-    process.exit(1);
+    fastify?.log.error(err);
+    server?.shutdown();
   }
+  process.on("uncaughtException", (error: Error) => {
+    fastify?.log.error("Uncaught Error",error);
+    server?.shutdown()
+  });
+
+  process.on("SIGINT", (signal: NodeJS.Signals) => {
+    fastify?.log.info(`Received ${signal} signal.`);
+    server?.shutdown();
+  });
+
+  process.on("SIGTERM", (signal: NodeJS.Signals) => {
+    fastify?.log.info(`Received ${signal} signal.`);
+    server?.shutdown()
+  });
+
+  process.on("unhandledRejection", (reason: string) => {
+    fastify?.log.error(`Unhandled Promise: ${reason}.`);
+    server?.shutdown();
+  });
 }
 
 main();
