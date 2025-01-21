20db4a80c6f49ae688b060535dc6b3f5b12d2728
ali <muhammadalihassanoffiial@gmail.com>
2025-01-21 19:43:14 +0500

    feat: add user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,26 @@
-import { fastify } from "../../app/app";
-import { CreateUserBody } from "./user.schemas";
+import Repository from "../../core/repository/IRepository";
+import { DuplicateException } from "../../lib/exception";
+import { User } from "./user.schemas";
+import bcrypt from "bcrypt";
 
-
-
-class UserService{
-
-  public async createUser(userInput: CreateUserBody) {
-    return userInput;
+class UserService {
+  public constructor(private readonly userRepository: Repository<User>) { };
+  
+  public async createUser(userInput: User) {
+    const { password, ...rest } = userInput;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const duplicate = await this.userRepository.find("email", rest.email);
+    if (duplicate) {
+      throw new DuplicateException("User already exits");
+    }
+    const user = await this.userRepository.insert({...rest,password:hashPassword})
+    return user;
   }
+
+  public async getUser(id:number|string) {
+    return await this.userRepository.findById(id);
+  }
+
 }
 
-
-
-
-export default new UserService();
+export default UserService;
