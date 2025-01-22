3463735216f0e48ea40c95e38418eeb505f3bd0f
ali <muhammadalihassanoffiial@gmail.com>
2025-01-22 09:04:26 +0500

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+      "@typescript-eslint/array-type": ["error", { default: "array" }],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/require-await": "off"
+    },
+  },
+  {
+    ignores: ["build/**", "**/*.mjs", "**/*.js", "**/*.config.ts"],
+  },
+  {
+    files: ["**/*.js"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  eslintPluginPrettierRecommended,
+);
