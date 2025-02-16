b38b9766dbaa394a73a28927c61f77655ab4ab01
ali <muhammadalihassanoffiial@gmail.com>
2025-02-16 13:05:09 +0500

    feat: add findOne user by given criteria in user repositary

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,27 +1,23 @@
 import { DatabaseInstance, User } from "@database/types.js";
-import { BaseRepository } from "base/repository/baseRepository.js";
+import { Repository } from "base/repository/baseRepository.js";
 
-class UserRepository extends BaseRepository<"user"> {
+class UserRepository extends Repository<"user"> {
   public constructor(db: DatabaseInstance) {
     super(db, "user");
   }
 
-  public async find(criteria: Partial<User>): Promise<User[]> {
+  public async findOne(criteria: Partial<Pick<User, "email" | "id">>) {
     let query = this.db.selectFrom("user");
-
-    if (criteria.id) {
-      query = query.where("id", "=", criteria.id);
-    }
-
-    if (criteria.name) {
-      query = query.where("name", "=", criteria.name);
-    }
 
     if (criteria.email) {
       query = query.where("email", "=", criteria.email);
     }
 
-    return await query.selectAll().execute();
+    if (criteria.id) {
+      query = query.where("id", "=", criteria.id);
+    }
+
+    return await query.selectAll().executeTakeFirst();
   }
 }
 
